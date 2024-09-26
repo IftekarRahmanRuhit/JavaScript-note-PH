97f29968e64b44313eb8e26d116002636444007f
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-09-26 22:16:06 +0600

    chain js

---
+++ ES6 More/chain.js
@@ -0,0 +1,11 @@
+
+const product = {
+    count : 500,
+    data : [
+        { id:1, name : 'Ruhit', age: 21 },
+        { id:2, name : 'Arham', age: 2 }
+    ]
+
+}
+// second person age 
+console.log(product.data[1].price);
