142a6bbbad945ff7d04be926e7d6e8ccb3759be1
IftekarRahmanRuhit <iftekarrahmanruhit2003@gmail.com>
2024-10-01 21:36:52 +0600

    api

--- Explore Api/js/app.js
+++ Explore Api/js/app.js
@@ -1,7 +1,12 @@
+function loadData2(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
 
 function loadUsers(){
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
+    .then(res => res.json())
     .then(data => displayUsers(data))
 }
 
